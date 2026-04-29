dc3a39ce93f35a5233e33d02dbba6104d0f0cfdf
Jianyun Yang <24181422@student.uwa.edu.au>
2026-04-29 23:37:37 +0800

    added python file for data transformation

--- dags/datwarehouse/data_utils.py
+++ dags/datwarehouse/data_utils.py
@@ -1,22 +1,18 @@
-from airflow.provider.postgres.hooks.postgres import PostgresHook
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from psycopg2.extras import RealDictCursor
 
-from psychopg2 import RealDictCursor
-
-table= 'ty_api'
+table = 'yt_api'
 
 def get_conn_cursor():
-    hook = PostgresHook(postgres_conn_id='  AIRFLOW_CONN_POSTGRES__DB_YT_ETL', databse='etl_db')
+    hook = PostgresHook(postgres_conn_id='AIRFLOW_CONN_POSTGRES__DB_YT_ETL', database='etl_db')
 
     conn = hook.get_conn()
-
     cur = conn.cursor(cursor_factory=RealDictCursor)
 
     return conn, cur
 
 def close_conn_cursor(conn, cur):
-
     cur.close()
-
     conn.close()
 
 def create_schema(schema):
@@ -25,9 +21,7 @@ def create_schema(schema):
     schema_sql = f"CREATE SCHEMA IF NOT EXISTS {schema}"
 
     cur.execute(schema_sql)
-
     conn.commit()
-
     close_conn_cursor(conn, cur)
 
 def create_table(schema):
@@ -36,37 +30,33 @@ def create_table(schema):
     if schema == 'staging':
         table_sql = f"""
         CREATE TABLE IF NOT EXISTS {schema}.{table} (
-            'Video_Id' TEXT PRIMARY KEY NOT NULL,
-            'Video_Title' TEXT NOT NULL,
-            'Upload_Date' TIMESTAMP NOT NULL,
-            'Duration' VARCHAR(20) NOT NULL,
-            'Video_Views' INTEGER,
-            'Like_Count' INTEGER,
-            'Comment_Count' INTEGER
+            "Video_Id" TEXT PRIMARY KEY NOT NULL,
+            "Video_Title" TEXT NOT NULL,
+            "Upload_Date" TIMESTAMP NOT NULL,
+            "Duration" VARCHAR(20) NOT NULL,
+            "Video_Views" INTEGER,
+            "Like_Count" INTEGER,
+            "Comment_Count" INTEGER
         )
         """
     else:
         table_sql = f"""
         CREATE TABLE IF NOT EXISTS {schema}.{table} (
-            'Video_Id' TEXT PRIMARY KEY NOT NULL,
-            'Video_Title' TEXT NOT NULL,
-            'Upload_Date' TIMESTAMP NOT NULL,
-            'Duration' VARCHAR(20) NOT NULL,
-            'Video_Views' INTEGER,
-            'Like_Count' INTEGER,
-            'Comment_Count' INTEGER
+            "Video_Id" TEXT PRIMARY KEY NOT NULL,
+            "Video_Title" TEXT NOT NULL,
+            "Upload_Date" TIMESTAMP NOT NULL,
+            "Duration" VARCHAR(20) NOT NULL,
+            "Video_Views" INTEGER,
+            "Like_Count" INTEGER,
+            "Comment_Count" INTEGER
         )
         """
     cur.execute(table_sql)
-
     conn.commit()
-
     close_conn_cursor(conn, cur)
 
-def get_video_ids(cur,schema):
-
-    cur.execute(f"""SELECT 'Video_Id' FROM {schema}.{table}""")
-
+def get_video_ids(cur, schema):
+    cur.execute(f"""SELECT "Video_Id" FROM {schema}.{table}""")
     ids = cur.fetchall()
 
     videos_ids = [id['Video_Id'] for id in ids] 
